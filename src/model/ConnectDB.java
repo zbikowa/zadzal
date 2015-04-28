244a879a9600291c5c5ab3a91d918bfbe6e09e05
zbikowa <agnieszka.jasiu@gmail.com>
2015-04-28 16:27:13 +0200

    formatowanie kodu ConnectDB

--- src/model/ConnectDB.java
+++ src/model/ConnectDB.java
@@ -7,48 +7,50 @@ import java.sql.Statement;
 
 public class ConnectDB {
 	Connection c = null;
-    Statement stmt = null;
-	
-    // wywo³anie metod 
-    public static void main( String args[] )
-    {
-    	ConnectDB db = new ConnectDB();
-    	db.initConnection();
-    	
-    	db.dropTableBookstore();
-    	db.createTableBookstore();
-    	db.addRecordToBookstore(1, "Z Jav¹ przez œwiat", "Aga Jasiu", "Nowa Era", 2001);
-    	
-    	db.closeConnection();
-    }
-    
-    //deklaracja metody po³¹czenia z baz¹ danych 
-    public void initConnection() {
-    	try {
+	Statement stmt = null;
+
+	// wywo³anie metod
+	public static void main(String args[]) {
+		ConnectDB db = new ConnectDB();
+		db.initConnection();
+
+		db.dropTableBookstore();
+		db.createTableBookstore();
+		db.addRecordToBookstore(1, "Z Jav¹ przez œwiat", "Aga Jasiu",
+				"Nowa Era", 2001);
+
+		db.closeConnection();
+	}
+
+	// deklaracja metody po³¹czenia z baz¹ danych
+	public void initConnection() {
+		try {
 			Class.forName("org.sqlite.JDBC");
 			this.c = DriverManager.getConnection("jdbc:sqlite:test.db");
-	    	this.stmt = this.c.createStatement();
-	    	System.out.println("Opened database successfully");
+			this.stmt = this.c.createStatement();
+			System.out.println("Opened database successfully");
 		} catch (Exception e) {
-			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-		    System.exit(0);
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
 		}
-    	
-    }
-    //deklaracja metody zamkniêcia po³¹czenia z  baz¹ danych
-    public void closeConnection() {
-    	try {
+
+	}
+
+	// deklaracja metody zamkniêcia po³¹czenia z baz¹ danych
+	public void closeConnection() {
+		try {
 			this.stmt.close();
 			this.c.close();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    }
-	  // usuniêcie bazy
-    public void dropTableBookstore() {
+	}
+
+	// usuniêcie bazy
+	public void dropTableBookstore() {
 		String sql = "DROP TABLE BOOKSTORE";
-		
+
 		try {
 			this.stmt.executeUpdate(sql);
 		} catch (SQLException e) {
@@ -56,33 +58,43 @@ public class ConnectDB {
 			e.printStackTrace();
 		}
 	}
-     //stworzenie table w bazie danych
-	  public void createTableBookstore() {
-		    try {
-		      String sql = "CREATE TABLE BOOKSTORE " +
-		                   "(ID INT PRIMARY KEY     NOT NULL," +
-		                   " nazwa          TEXT    NOT NULL, " + 
-		                   " autor          TEXT     NOT NULL, " + 
-		                   " wydawnictwo    TEXT     NOT NULL," + 
-		                   " rokWydania        INT)"; 
-		      this.stmt.executeUpdate(sql);
-		    } catch ( Exception e ) {
-		      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-		      System.exit(0);
-		    }
-		    System.out.println("Table created successfully");
-	  }
-	  //dodanie rekordu do tabeli w bazie 
-	  public void addRecordToBookstore(int id, String nazwa, String autor, String wydawnictwo, int rokWydania){
-		   try {
-			 String sql = "INSERT INTO BOOKSTORE (ID, nazwa, autor, wydawnictwo, rokWydania)" +
-					 		"VALUES (" + id + ",'" + nazwa + "','" + autor + "','" + wydawnictwo + "'," + rokWydania +")";
-			 this.stmt.executeUpdate(sql);
-			 
-		   }catch ( Exception e ) {
-			      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-			      System.exit(0);
-			    }
-			    System.out.println("Records created successfully");
-	  }
+
+	// stworzenie table w bazie danych
+	public void createTableBookstore() {
+		try {
+			String sql = "CREATE TABLE BOOKSTORE "
+					+ "(ID INT PRIMARY KEY     NOT NULL,"
+					+ " nazwa          TEXT    NOT NULL, "
+					+ " autor          TEXT     NOT NULL, "
+					+ " wydawnictwo    TEXT     NOT NULL,"
+					+ " rokWydania        INT)";
+			this.stmt.executeUpdate(sql);
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+		System.out.println("Table created successfully");
+	}
+
+	// dodanie rekordu do tabeli w bazie
+	public void addRecordToBookstore(int id, String nazwa, String autor,
+			String wydawnictwo, int rokWydania) {
+		try {
+			String sql = "INSERT INTO BOOKSTORE (ID, nazwa, autor, wydawnictwo, rokWydania)"
+					+ "VALUES ("
+					+ id
+					+ ",'"
+					+ nazwa
+					+ "','"
+					+ autor
+					+ "','"
+					+ wydawnictwo + "'," + rokWydania + ")";
+			this.stmt.executeUpdate(sql);
+
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+		System.out.println("Records created successfully");
+	}
 }
